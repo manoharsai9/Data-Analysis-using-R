12cd560417b6dc1f1c13edaec1431a2efe21f504
manoharsai5 <jasti.manoharsai@gmail.com>
2019-03-08 10:28:52 -0600

    update skew

---
+++ Exploratory Data Analysis/Skew_Solution.R
@@ -0,0 +1,30 @@
+### Visual examination of the skew
+
+sk1 <- read_csv("skew_1.csv")
+sk2 <- read_csv("skew_2.csv")
+
+skew1 <- ggplot(data = sk1, aes(x = `Dataset 1`))
+skew1 + geom_histogram(binwidth = 100,
+                       color = "darkslategray",
+                       fill = "darkslategray4",
+                       alpha = 0.5) +
+  theme_light()
+
+# The skew of this dataset is positive
+
+
+skew2 <- ggplot(data = sk2, aes(x = `Dataset 2`))
+skew2 + geom_histogram(binwidth = 100,
+                       color = "darkslategray",
+                       fill = "darkslategray4",
+                       alpha = 0.5) +
+  theme_light()
+
+# The skew of this dataset is negative
+
+### Numerical examination of the skew
+
+library(psych)
+
+describe(sk1)
+describe(sk2)
